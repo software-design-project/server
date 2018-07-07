736e2ffde1914eaf7955beaf0f681b61a0fc0bff
liaozelin <489531912@qq.com>
2018-07-07 14:41:50 +0800

    pprof
    Change-Id: I27f437d91a0d241ed9135ed5274c9c0d9857bdcd

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	_ "net/http/pprof"
 
 	"./configs"
 	"./controllers"
@@ -22,6 +23,11 @@ func main() {
 	// 由于使用的是相对路径，程序需要运行在本文件所在目录下
 	fsh := http.FileServer(http.Dir("../public/"))
 	http.Handle("/public/", http.StripPrefix("/public/", fsh))
+
+	go func() {
+		fmt.Println("pprof")
+		http.ListenAndServe("localhost:12345", nil)
+	}()
 
 	fmt.Printf("start server at %v...\n", addr)
 	http.ListenAndServe(addr, nil)
